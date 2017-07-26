97097c86bbe8eb01b0497b7b53bac358f0b08838
Bilal Itani <bilalitani1@gmail.com>
2017-07-26 13:46:26 -0400

    Add non-functioning mockup scene

---
+++ Assets/Scripts/UIElementDragger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Copyright (C) Xenfinity LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Bilal Itani <bilalitani1@gmail.com>, June 2017
+ */
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public const LayerMask draggableLayers;
+
+    #region Monobehaviour API
+
+	void Update () {
+		
+	}
+
+    #endregion
+}
